c06d12a6f0ae49b9a02a3070a9ad267eeb73adc8
juan carlos raya <Juank@iMac-de-juan.local>
2023-03-22 10:01:33 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect, useState, useRef } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+const [page, setPage] = useState(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])  
+const [cargando, setCargando] = useState(true)
+
+const prueba = useRef(null)
+
+
+const callback = (entradas) => {
+  if(entradas[0].isIntersecting){
+    prueba.current.src = "/10.jpg"
+    setCargando(false)
+  }
+ 
+}
+
+useEffect(()=>{
+
+ const observador = new IntersectionObserver(callback, {
+  rootMargin: "0px 0px 50px 0px",
+  threshold: 0.1
+ }) 
+
+ observador.observe(prueba.current)
+
+},[prueba])
+
+
   return (
     <>
       <Head>
@@ -20,103 +46,75 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+          <button
+            onClick={()=>{
+              add()
+            }}
+           >Add</button>
+         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+      
+      
+      
+       <div className={styles.center}>
+        <Image
+         src={'/1.jpg'}
+         className={styles.logo}
+         alt="Next.js Logo"
+         width={500}
+         height={500}
+         priority
+        />
+     </div>
+          
+     <div className={styles.center}>
+        <Image
+         src={'/1.jpg'}
+         className={styles.logo}
+         alt="Next.js Logo"
+         width={500}
+         height={500}
+         priority
+        />
+     </div>
+          
+     <div className={styles.center}>
+        <Image
+         src={'/1.jpg'}
+         className={styles.logo}
+         alt="Next.js Logo"
+         width={500}
+         height={500}
+         priority
+        />
+     </div>
+          
+     <div className={styles.center}>
+        <Image
+         src={'/1.jpg'}
+         className={styles.logo}
+         alt="Next.js Logo"
+         width={500}
+         height={500}
+         priority
+        />
+     </div>
+     <div>
+     {cargando ? <h2>Cargando...</h2> : <h2>Cargada</h2>}
+     </div>
+     
+     <div className={styles.center}>
+         <Image
+         ref={prueba}
+         className={styles.logo}
+         alt="Next.js Logo"
+         width={500}
+         height={500}
+         priority
+        />
+     </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      
       </main>
     </>
   )
